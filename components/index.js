956c39a6536d0e6c28d7a68bd6920f8e04d2ba78
dude <hsn.safdari12@gmail.com>
2020-11-26 21:51:56 +0100

    new changes1

--- components/index.js
+++ components/index.js
@@ -1,9 +1,4 @@
 <html>
-<<<<<<< HEAD
     <head><title>index page Branch temp</title></head>
         <h5>Hello world from index</h5>
 </html>
-=======
-    <head><title>index page </title></head>
-</html>
->>>>>>> parent of e201996... push to temp branch
